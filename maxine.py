03bba783ddc136c1a8e2ee1d56b7362dd446183b
Jade O'Neill <jadeoneillpk@gmail.com>
2022-06-30 22:53:44 -0400

    Now monsters explode when Maxine touches them!

--- maxine.py
+++ maxine.py
@@ -9,11 +9,13 @@ MAXINE_START = (100, 56)
 
 maxine = Actor('maxine')
 maxine.pos = MAXINE_START
+maxine_sprite_name = 'maxine'
 
 pore = Actor('pore')
 pore.pos = (WIDTH/2, HEIGHT/2)
 
 cells = set()
+dead_cells = set()
 
 score = 0
 
@@ -21,8 +23,14 @@ def draw():
     screen.fill((128, 128, 0))
     maxine.draw()
     pore.draw()
+    
+    # Draw either a cell or the explosion
     for cell in cells:
-    	cell.draw()
+        screen.blit(cell.sprite_name, (cell.x, cell.y))
+
+    for cell in dead_cells:
+        screen.blit(cell.sprite_name, (cell.x, cell.y))
+
     screen.draw.text('SCORE ' + str(score), (10, 10))
 
 def update():
@@ -54,6 +62,18 @@ def update():
     
     for cell in to_remove:
         cells.remove(cell)
+        kill_cell(cell)
+    
+    # Make dead cells disappear after a certain amount of time.
+    to_remove = []
+    for cell in dead_cells:
+        cell.disappear_timer -= 1
+        
+        if cell.disappear_timer <= 0:
+            to_remove.append(cell)
+            
+    for cell in to_remove:
+        remove_dead_cell(cell)
     
     #if maxine.left > WIDTH:
     #    maxine.right = 0
@@ -75,6 +95,7 @@ def add_cell():
     cell_type = random.choice(['monster1_right', 'monster2', 'monster3', 'monster4',
         'monster5', 'monster6', 'monster7', 'monster8', 'monster9', 'monster10'])
     cell = Actor(cell_type)
+    cell.sprite_name = cell_type
     cell.pos = (pore.x, pore.y)
 	# custom parameters
     cell.deltax = random.randrange(-2, 3)
@@ -85,7 +106,25 @@ def add_cell():
 	
     cells.add(cell)
 
-    clock.schedule_unique(add_cell, 7)
+    clock.schedule_unique(add_cell, 7.0)
+
+def kill_cell(cell):
+    #print('kill_cell('+cell(str)+')')
+    dead_cells.add(cell)
+    cell.sprite_name = 'explosion1'
+    # Set a disappear timer in frames.
+    cell.disappear_timer = 15
+ 
+    # Don't know why this good code doesn't work. remove_dead_cell never gets called.
+    
+#    def remove_this_dead_cell():
+#        remove_dead_cell(cell)
+#        
+#    clock.schedule_unique(remove_this_dead_cell, 0.5)
+
+def remove_dead_cell(cell):
+    #print('remove_dead_cell('+str(cell)+')')
+    dead_cells.remove(cell)
 
 clock.schedule_unique(add_cell, 4.0)   
 
