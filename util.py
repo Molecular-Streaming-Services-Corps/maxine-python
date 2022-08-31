8bf7465e0b921f4ae694a883341e18bc8b4998ac
Jade O'Neill <jadeoneillpk@gmail.com>
2022-08-31 19:29:09 +1200

    Add functions for polar coordinates

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def process_joystick_data(joystick_data):
     binary_string = bin(joystick_data)[2:]
     return process_joystick_string(binary_string)
@@ -16,3 +18,13 @@ def process_joystick_string(binary_string):
                 
     return pressed
 
+# Polar coordinate functions
+def cart2pol(x, y):
+    r = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+    return(r, theta)
+
+def pol2cart(r, theta):
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    return(x, y)
